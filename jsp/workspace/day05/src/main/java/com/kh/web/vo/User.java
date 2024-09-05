d79c6fd801d6bff969f115376591396153eca7ad
sw2park <pswoo990803@gmail.com>
2024-09-05 16:02:14 +0900

    24.09.05 day05 jsp File Upload

---
+++ jsp/workspace/day05/src/main/java/com/kh/web/vo/User.java
@@ -0,0 +1,15 @@
+package com.kh.web.vo;
+
+public class User {
+	public String userid;
+	public String username;
+	public int age;
+	
+	public User(String userid, String username, int age) {
+		super();
+		this.userid = userid;
+		this.username = username;
+		this.age = age;
+	}
+
+}
