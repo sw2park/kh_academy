297449085184569e0070073701dd5bc7e5f7e76a
sw2park <pswoo990803@gmail.com>
2024-10-31 12:52:58 +0900

    24.10.31 day14 querydsl 세팅

---
+++ spring/workspace/querydsl/src/main/java/com/codingbox/querydsl/queryDSLMain.java
@@ -0,0 +1,31 @@
+package com.codingbox.querydsl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class queryDSLMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf
+		= Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		// transaction : 하나의 작업단위
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		try {
+		
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			em.close();
+			emf.close();
+		}
+		
+	}
+
+}
